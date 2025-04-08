d608e5f9a9269b3fec62a40a474fe502421afee4
pcha07 <86342766+pcha07@users.noreply.github.com>
2025-04-07 17:40:32 -0700

    crawl page recursive

--- crawl.js
+++ crawl.js
@@ -1,26 +1,49 @@
 const { JSDOM } = require("jsdom");
 
-async function crawlPage(current_url) {
+async function crawlPage(base_url, current_url, pages) {
+
+  const base_url_obj = new URL(base_url);
+  const current_url_obj = new URL(current_url);
+
+  if(base_url_obj.hostname !== current_url_obj.hostname) {
+    return pages;
+  }
+
+  const normalized_curr_url = normalizeURL(current_url);
+  if(pages[normalized_curr_url > 0]) {
+    pages[normalized_curr_url]++;
+    return pages;
+  }
+
+  pages[normalized_curr_url] = 1;
   console.log(`crawling: ${current_url}}`);
+
 
   try {
     const resp = await fetch(current_url);
 
     if (resp.status > 399) {
       console.log(`error in fetch with status code: ${resp.status} on page ${current_url}`);
-      return;
+      return pages;
     }
 
     const content_type = resp.headers.get("content-type");
     if(!content_type.includes("text/html")) {
       console.log(`non-html response, content type: ${content_type} on page ${current_url}`);
-      return;
+      return pages;
     }
 
-    console.log(await resp.text());
+    const html_body = await resp.text();
+
+    const next_urls = getURLsFromHTML(html_body, base_url);
+
+    for(const next_url of next_urls) {
+      pages = await crawlPage(base_url, next_url, pages);
+    }
   } catch (error) {
     console.log(`error in fetch: ${error}, on page: ${current_url}`);
   }
+  return pages;
 }
 
 function getURLsFromHTML(htmlBody, baseURL) {
